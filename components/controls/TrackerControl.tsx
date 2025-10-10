28e2836d4c2e3d831213a4f644ead4e61e707c06
Alex Bo <sir.ejji@gmail.com>
2025-10-10 18:30:28 +0400

    Add @tonaljs/chord package and integrate chord detection in TrackerControl
    - Added @tonaljs/chord as a dependency to enable chord detection functionality.
    - Enhanced TrackerControl component to utilize MusicRNN for music generation and integrated chord detection using the Tonal library.
    - Implemented a new button for generating music based on the current grid, improving user interaction and functionality.

--- components/controls/TrackerControl.tsx
+++ components/controls/TrackerControl.tsx
@@ -1,4 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import { MusicRNN } from '@magenta/music/es6/music_rnn';
+import * as Tonal from 'tonal';
+import { detect } from "@tonaljs/chord";
+
 
 interface TrackerControlProps {
   rows: number;
@@ -16,6 +20,16 @@ const TrackerControl: React.FC<TrackerControlProps> = ({
   activeRow,
 }) => {
   const [grid, setGrid] = useState<string[][]>([]);
+
+  const rnn = useMemo(() => new MusicRNN(
+    // 'https://storage.googleapis.com/download.magenta.tensorflow.org/tfjs_checkpoints/music_rnn/chord_pitches_improv'
+    'https://storage.googleapis.com/download.magenta.tensorflow.org/tfjs_checkpoints/music_rnn/basic_rnn'
+  ), []);
+
+  useEffect(() => {
+    rnn?.initialize();
+  }, [rnn]);
+
   useEffect(() => {
     const newGrid = Array.from({ length: rows }, (_, r) =>
       Array.from({ length: cols }, (_, c) => {
@@ -72,6 +86,67 @@ const TrackerControl: React.FC<TrackerControlProps> = ({
     }
   };
 
+  const handleGenerate = async () => {
+    let step = 1;
+    const notes = grid
+    .flatMap((row, rowIndex) => row.map((cell, colIndex) => {
+      const dur = 1;
+      const note = {
+        pitch: Tonal.Note.midi(cell),
+        quantizedStartStep: step,
+        quantizedEndStep: step + dur
+      };
+      step += dur;
+      return note;
+    }))
+    .filter(note => note.pitch !== null);
+    
+    const seedSeq = {
+      totalQuantizedSteps: notes[notes.length - 1].quantizedEndStep,
+      quantizationInfo: {
+        stepsPerQuarter: 2
+      },
+      notes,
+    };
+
+    const chord = detect(notes.map(n => Tonal.Note.pc(Tonal.Note.fromMidi(n.pitch))));
+   
+    console.log(notes.map(n => Tonal.Note.pc(Tonal.Note.fromMidi(n.pitch))), chord)
+
+    const genSeq = await rnn.continueSequenceAndReturnProbabilities(seedSeq, 8, 0.5)
+
+    // if (!genSeq.notes) {
+    //   return;
+    // }
+    console.log(genSeq);
+    console.log(genSeq.probs.map(p => p.reduce((acc, curr) => acc > curr ? acc : curr , 0)));
+    // TransportTime, ("4:3:2") will also provide tempo and time signature relative times in the form BARS:QUARTERS:SIXTEENTHS
+    //   const part = new Tone.Part(((time, note) => {
+    //     // the notes given as the second element in the array
+    //     // will be passed in as the second argument
+    //     synth.triggerAttackRelease(note, "8n", time);
+    // }), [[0, "C2"], ["0:2", "C3"], ["0:3:2", "G2"]]).start(0);
+    // const stepsPerQuarter = genSeq?.quantizationInfo?.stepsPerQuarter || 1;
+
+    // const generatedSequence = genSeq.notes
+    //   .filter(n => typeof n.quantizedStartStep === 'number')
+    //   .map(n => ({
+    //     time: { "4n": n.quantizedStartStep / stepsPerQuarter },
+    //     note: n
+    //   }));
+
+    // const part = new Part(((time: number, note) => {
+    //   // тут нужно тригерить ноту или отправлять парт в аутпут
+    // }), generatedSequence).start(0);
+
+
+    // generatedSequence = generatedSequence.concat(this.seqToTickArray(genSeq));
+    // setTimeout(generateNext, generationIntervalTime * 1000);
+    // });
+    // }
+    // };
+  };
+
   const styles: { [key: string]: React.CSSProperties } = {
     container: {
       fontFamily: 'monospace',
@@ -112,6 +187,9 @@ const TrackerControl: React.FC<TrackerControlProps> = ({
         </button>
         <button onClick={handlePaste} style={styles.button}>
           Paste
+        </button>
+        <button onClick={handleGenerate} style={styles.button}>
+          Generate
         </button>
       </div>
       {grid.map((row, rowIndex) => (
