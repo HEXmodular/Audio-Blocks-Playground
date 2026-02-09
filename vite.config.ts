9d32affe973e097a9456206e90ab075f31dff4c8
Alex Bo <sir.ejji@gmail.com>
2026-02-09 20:34:02 +0400

    added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -23,6 +25,9 @@ export default defineConfig(({ mode }) => {
           '@blocks': path.resolve(__dirname, './blocks'),
           '@stores': path.resolve(__dirname, './stores'),
         }
-      }
+      },
+      plugins: [
+        tailwindcss(),
+      ],
     };
 });
