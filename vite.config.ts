45e5bf1337d0fe9a1a4af41b7c339035315401bd
Alex Bo <sir.ejji@gmail.com>
2026-02-22 00:09:02 +0400

    added pwa feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
-
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -28,6 +28,42 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [
         tailwindcss(),
+        VitePWA({
+          workbox: {
+            // Увеличиваем лимит до 5 MiB (5 * 1024 * 1024)
+            maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+          },
+          // 1. Стратегия регистрации (автоматическое обновление)
+          registerType: 'autoUpdate',
+          
+          // 2. Включаем во время разработки (опционально)
+          devOptions: {
+            enabled: true 
+          },
+    
+          // 3. Настройка манифеста (то, что увидит Android)
+          manifest: {
+            name: '',
+            short_name: 'ABlocks',
+            description: 'Audio Blocks',
+            theme_color: '#ffffff',
+            background_color: '#ffffff',
+            display: 'standalone', // Запуск без адресной строки
+            icons: [
+              {
+                src: './icons/pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+              },
+              {
+                src: './icons/pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any maskable' // Важно для Android (адаптивные иконки)
+              }
+            ]
+          }
+        })
       ],
     };
 });
